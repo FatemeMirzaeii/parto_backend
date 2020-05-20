dc88af5fd5c19460e1b111c9b44635071fd2752e
Fateme Mirzaei <fatememirzaie@live.com>
2020-05-20 09:40:43 +0430

    x-forwarded-for

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ router.post("/signIn", async (req, res) => {
   const token = usr.generateAuthToken();
   res.header("x-auth-token", token).status(200).json({ data: { id: usr.id } });
   await user_log.create({
-    IP: req.connection.remoteAddress,
+    IP: req.header("x-forwarded-for"),
     version: req.body.version,
     login_date: Date.now(),
   });
