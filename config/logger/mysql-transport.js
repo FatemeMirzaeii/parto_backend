a4fef932e1a7b877e5975df9d0a0b552a3aff5c0
Fateme Mirzaei <fatememirzaie@live.com>
2020-06-25 19:23:48 +0430

    correcting mysql log and somevalidations.

--- config/logger/mysql-transport.js
+++ config/logger/mysql-transport.js
@@ -1,21 +1,21 @@
-const Transport = require('winston-transport');
+const Transport = require("winston-transport");
 const { log } = require("../../models");
 
 class Mysql_tranport extends Transport {
-    constructor(opts) {
-        super(opts);
-    }
-    async log(info, callback) {
-        await log.create({
-            level: info.level,
-            message: info.message,
-            timestamp: info.timestamp,
-        });
-        setImmediate(() => {
-            this.emit('logged', info);
-        });
-        callback();
-    }
-};
+  constructor(opts) {
+    super(opts);
+  }
+  async log(info, callback) {
+    await log.create({
+      level: info.level,
+      message: info.message,
+      time: info.timestamp,
+    });
+    setImmediate(() => {
+      this.emit("logged", info);
+    });
+    callback();
+  }
+}
 
 module.exports = Mysql_tranport;
--- models/User.js
+++ models/User.js
@@ -1,50 +1,58 @@
-'use strict';
+"use strict";
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const secret = fs.readFileSync("../private.key", "utf8");
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('user', {
-    name: {
-      type: DataTypes.STRING,
-      validate: {
-        max: 128,
-        notEmpty: {
-          msg: "لطفا نام خود را وارد کنید.",
+  const User = sequelize.define(
+    "user",
+    {
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          max: 128,
+          notEmpty: {
+            msg: "لطفا نام خود را وارد کنید.",
+          },
         },
       },
-    },
-    // username: {
-    //   type: DataTypes.STRING,
-    //   unique: true,
-    // },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-      validate: {
-        min: 5,
-        max: 255,
-        notNull: true,
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      validate: {
-        max: 1024,
-        notEmpty: {
-          msg: "لطفا رمز خود را وارد کنید.",
+      // username: {
+      //   type: DataTypes.STRING,
+      //   unique: true,
+      // },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          min: 5,
+          max: 255,
+          notNull: {
+            msg: "لطفا ایمیل خود را وارد کنید.",
+          },
+          isEmail: {
+            msg: "ایمیل معتبر نیست.",
+          },
         },
       },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          max: 1024,
+          notEmpty: {
+            msg: "لطفا رمز خود را وارد کنید.",
+          },
+        },
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+      },
     },
-    active: {
-      type: DataTypes.BOOLEAN,
-    },
-  }, {
-    freezeTableName: true,
-    underscored: true,
-  });
+    {
+      freezeTableName: true,
+      underscored: true,
+    }
+  );
   User.associate = function (models) {
     User.belongsTo(models.role, {
       onDelete: "RESTRICT",
@@ -54,10 +62,10 @@ module.exports = (sequelize, DataTypes) => {
     });
     User.belongsToMany(models.category, {
       through: "user_favorite_category",
-    })
+    });
   };
   User.prototype.generateAuthToken = () => {
     return jwt.sign({ _id: this.id }, secret);
-  }
+  };
   return User;
 };
