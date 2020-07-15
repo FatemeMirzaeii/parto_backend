4b5748bd74e790d622ae492d41c9d5c819723e7f
zand <zzand7755@gmail.com>
2020-07-15 18:09:17 +0430

    change

--- routes/user.js
+++ routes/user.js
@@ -4,42 +4,42 @@ const bcrypt = require("bcrypt");
 const router = express.Router();
 const translate = require("../config/translate");
 
-// /**
-//  * @swagger
-//  * /signUp:
-//  *   post:
-//  *     tags:
-//  *       - Users
-//  *     name:sign up
-//  *     summary: sign up in a user
-//  *     produces:
-//  *       - application/json
-//  *     consumes:
-//  *       - application/json
-//  *     parameters:
-//  *       - name: body
-//  *       - email: body
-//  *       - password: body
-//  *         schema:
-//  *           $ref: '#/definitions/User'
-//  *           type: object
-//  *           properties:
-//  *             name:
-//  *               type: string
-//  *             password:
-//  *               type: string
-//  *             email:
-//  *               type: string  
-//  *         required:
-//  *           - email
-//  *      responses:
-//  *       '200':
-//  *         description: User found and logged in successfully
-//  *       '401':
-//  *         description: Bad username, not found in db
-//  *       '403':
-//  *         description: Username and password don't match
-//  */
+/**
+ * @swagger
+ * /signUp:
+ *   post:
+ *     tags:
+ *       - Users
+ *     name:sign up
+ *     summary: sign up in a user
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *       - email: body
+ *       - password: body
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *           type: object
+ *           properties:
+ *             name:
+ *               type: string
+ *             password:
+ *               type: string
+ *             email:
+ *               type: string  
+ *         required:
+ *           - email
+ *      responses:
+ *       '200':
+ *         description: User found and logged in successfully
+ *       '401':
+ *         description: Bad username, not found in db
+ *       '403':
+ *         description: Username and password don't match
+ */
 
 router.post("/signUp/:lang", async (req, res) => {
   const exists = await user.findOne({
