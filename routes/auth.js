f9f8dfc448fbabb2abebf7b14641e46820c5c8d9
zand <zzand7755@gmail.com>
2020-07-25 00:09:40 +0430

    fix (verification,forget password,change password)

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const { uuid } =require ('uuid');
+const { uuid } = require('uuidv4');
 const express = require("express");
 const { user } = require("../models");
 const bcrypt = require("bcrypt");
@@ -31,8 +31,9 @@ router.post("/emailForgetPassword/:lang",async(req,res)=>{
     },
   });
   if (!usr) return res.status(400).json({ message: await translate("INVALIDENTRY", req.params.lang) });
-  const uuid=uuid()
-  await usr.update({ uuid: uuid , forgetPasswordDate:Date.now() });
+  
+  
+  await usr.update({ uuid: uuid()});
 
   await usr.createUser_log({
     i_p: req.header("x-forwarded-for"),
@@ -40,8 +41,8 @@ router.post("/emailForgetPassword/:lang",async(req,res)=>{
     login_date: Date.now(),
   });
   const subject='Forget password for use parto application';
-  const text='please click on the linke https://api.partobanoo.com/auth/forgetPassword/:'+ uuid+':/'+lang;
-  const result=sendEmail('info@partobanoo.com',req.body.email,text,subject);
+  const text='please click on the linke https://api.partobanoo.com/auth/forgetPassword/'+ usr.uuid+'/'+req.params.lang;
+  const result=sendEmail('parto@partobanoo.com',req.body.email,text,subject);
   if(result=="ERROR") return res.status(502).json({message: await translate("SERVERERROR", "fa")});
   else return res.status(200).json({ message: await translate("SUCCESSFUL", req.params.lang) });
 })
@@ -49,15 +50,14 @@ router.post("/emailForgetPassword/:lang",async(req,res)=>{
 router.post("/forgotPassword/:lang", async(req, res) => {
   const usr = await user.findOne({
     where: {
-      uuid: uuid,
+      uuid: req.body.uuid,
     },
   });
   if(!usr) return res.status(400).json({ message: await translate("INVALIDID", req.params.lang) });
-  
-  let time=dates.compare(Date.now(),usr.updated_at);//compare to time with each other +15 min
-  if(time>1)  return res.status(400).json({ message: await translate("INVALIDENTRY", req.params.lang) });
+ 
+  if(Math.abs(new Date().getTime() - usr.updatedAt)>900000)  return res.status(400).json({ message: await translate("INVALIDENTRY", req.params.lang) });
     
-  await usr.update({ password: req.body.newPassword });
+  await usr.update({ password:await bcrypt.hash(req.body.newPassword, 10) });
   await usr.createUser_log({
     i_p: req.header("x-forwarded-for"),
     version: req.body.version,
@@ -67,7 +67,7 @@ router.post("/forgotPassword/:lang", async(req, res) => {
   
 });
 
-router.post("/changePassword:lang", async(req, res) => {
+router.post("/changePassword/:lang", async(req, res) => {
   const usr = await user.findOne({
     where: {
       email: req.body.email,
@@ -78,7 +78,7 @@ router.post("/changePassword:lang", async(req, res) => {
   const pass = await bcrypt.compare(req.body.password, usr.password);
   if (!pass) return res.status(400).json({ message: await translate("WRONGPASSWORD", req.params.lang) });
   
-  await usr.update({ password: req.body.newPassword });
+  await usr.update({ password:await bcrypt.hash(req.body.newPassword, 10) });
 
   await usr.createUser_log({
     i_p: req.header("x-forwarded-for"),
@@ -89,13 +89,20 @@ router.post("/changePassword:lang", async(req, res) => {
 });
 
 router.post("/sendVerificationCode", async(req, res) => {
+  const usr = await user.findOne({
+    where: {
+      email: req.body.email,
+    },
+  });
+  if (!usr) return res.status(400).json({ message: await translate("INVALIDENTRY", "fa") });
+ 
   const code= Math.floor(Math.random() * (99999 - 10000 + 1) + 10000);
 
   const subject='Verification Code for parto application ';
-  let text = 'می باشد'+code +' کد فعالسازی شما '
-  const result=sendEmail(PartoEmail,Pass,req.body.email,subject,text);
-  if(result=="ERROR") return res.status(501);
-  else return res.status(200).json({ message: await translate("SUCCESSFUL", req.params.lang) })
+  let text =`کد فعالسازی شما : ${code}  `;
+  const result=sendEmail('parto@partobanoo.com',req.body.email,text,subject);
+  if(result=="ERROR") return res.status(502).json({message: await translate("SERVERERROR", "fa")});
+  else return res.status(200).json({ message: await translate("SUCCESSFUL", "fa") })
     
 });
 module.exports = router;
