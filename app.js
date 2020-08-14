db0b2cf8b65b05132e4e2f3e923d619eeded2977
Fateme Mirzaei <fatememirzaie@live.com>
2020-08-14 21:52:24 +0430

    express.static

--- app.js
+++ app.js
@@ -50,24 +50,22 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup()
 );
-app.configure("website", function () {
-  app.use(express.static(`../../Fattahi/deploy/production/build`));
-  app.use(express.static(`../../Fattahi/deploy/staging/build`));
-});
-app.use(
-  vhost("partobanoo.com", function (req, res) {
-    res.sendFile("index.html", {
-      root: "../../Fattahi/deploy/production/build",
-    });
-  })
-);
-app.use(
-  vhost("api.partobanoo.com", function (req, res) {
-    res.sendFile("index.html", {
-      root: "../../Fattahi/deploy/staging/build",
-    });
-  })
-);
+app.use(express.static(`../../Fattahi/deploy/production/build`));
+app.use("/staging", express.static(`../../Fattahi/deploy/staging/build`));
+// app.use(
+//   vhost("partobanoo.com", function (req, res) {
+//     res.sendFile("index.html", {
+//       root: "../../Fattahi/deploy/production/build",
+//     });
+//   })
+// );
+// app.use(
+//   vhost("api.partobanoo.com", function (req, res) {
+//     res.sendFile("index.html", {
+//       root: "../../Fattahi/deploy/staging/build",
+//     });
+//   })
+// );
 
 // app.get("/*", (req, res) => {
 //   res.sendFile("index.html", { root: "../../Fattahi/deploy/staging/build" });
