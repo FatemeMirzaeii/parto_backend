5201811246c0f3c8b8c8e3be3daeef2171daec3b
zand <zzand7755@gmail.com>
2020-08-23 17:52:49 +0430

    change

--- pwa.js
+++ pwa.js
@@ -3,11 +3,30 @@ require("./models/index");
 
 const express = require("express");
 const logger = require("./config/logger/logger");
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
 
-const  pwa = express();
+const next = require('next')
 
-pwa.use(express.static(`../../Fattahi/parto-pwa`));
-pwa.get("/*", (req, res) => {
-  res.sendFile("index.html", { root: "../../Fattahi/parto-pwa" });
-});
-pwa.listen(3925, () => logger.info("Listening on port 3925..."));
+const app = next({ })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '../../Fattahi/parto-pwa') {
+      app.render(req, res, '../../Fattahi/parto-pwa', query)
+    }
+  }).listen(3925, () => logger.info("Listening on port 3925..."));
+})
+
+// pwa.use(express.static(`../../Fattahi/parto-pwa`));
+// pwa.get("/*", (req, res) => {
+//   res.sendFile("index.html", { root: "../../Fattahi/parto-pwa" });
+// });
+// pwa.listen(3925, () => logger.info("Listening on port 3925..."));
