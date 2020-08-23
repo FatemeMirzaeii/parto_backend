69588aade419e70307f2fb708f06220ddb4b664e
zand <zzand7755@gmail.com>
2020-08-23 18:15:46 +0430

    change

--- pwa.js
+++ pwa.js
@@ -4,11 +4,21 @@ require("./models/index");
 const express = require("express");
 const logger = require("./config/logger/logger");
 
-const next = require('next')
-const pwa = next({})
+const next = require('next');
+const pwa = next({});
+const handle = app.getRequestHandler();
 
-pwa.use(express.static(`../../Fattahi/parto-pwa`));
-pwa.get("/*", (req, res) => {
-  res.sendFile("index.js", { root: "../../Fattahi/parto-pwa" });
+pwa.prepare().then(() => {
+    const server = express();
+    
+    server.use(express.static(`../../Fattahi/parto-pwa`));
+    server.get('*', (req, res) => {
+      return handle(req, res);
+    });
+    server.listen(3925, () => logger.info("Listening on port 3925..."));
 });
-pwa.listen(3925, () => logger.info("Listening on port 3925..."));
+//pwa.use(express.static(`../../Fattahi/parto-pwa`));
+// pwa.get("/*", (req, res) => {
+//   res.sendFile("index.js", { root: "../../Fattahi/parto-pwa" });
+// });
+
