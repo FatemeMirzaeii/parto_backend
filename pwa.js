6adec4711b583626d0acdd5c7c2824f356b0c5a8
zand <zzand7755@gmail.com>
2020-08-26 00:12:34 +0430

    change

--- pwa.js
+++ pwa.js
@@ -10,9 +10,9 @@ const handle = app.getRequestHandler();
 pwa.prepare().then(() => {
     const server = express();
     
-    server.use(express.static(`../../Fattahi/parto-pwa`));
+    //server.use(express.static(`../../Fattahi/parto-pwa`));
     server.get('*', (req, res) => {
-      res.sendFile("./next/server/pages/index.html", { root: "../../Fattahi//parto-pwa" });
+      //res.sendFile("./next/server/pages/index.html", { root: "../../Fattahi//parto-pwa" });
       return handle(req, res);
     });
     server.listen(3925, () => logger.info("Listening on port 3925..."));
