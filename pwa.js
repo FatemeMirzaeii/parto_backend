4600b6f49115d6ebace62a9c35679b20dfe7b653
zand <zzand7755@gmail.com>
2020-08-25 16:39:09 +0430

    change

--- pwa.js
+++ pwa.js
@@ -1,19 +1,14 @@
+
 require("express-async-errors");
 require("./models/index");
 
 const express = require("express");
 const logger = require("./config/logger/logger");
-const next = require('next');
-const pwa = next({});
-const handle = app.getRequestHandler();
 
-pwa.prepare().then(() => {
-    const server = express();
-    
-    //server.use(express.static(`../../Fattahi/parto-pwa`));
-    server.get('*', (req, res) => {
-      return handle(req, res);
-    });
-    server.listen(3925, () => logger.info("Listening on port 3925..."));
+const developmentApp = express();
+
+developmentApp.use(express.static(`../../Fattahi/parto-pwa`));
+developmentApp.get("/*", (req, res) => {
+  res.sendFile("index.js", { root: "../../Fattahi/parto-pwa" });
 });
-
+developmentApp.listen(3925, () => logger.info("Listening on port 3925..."));
