0b101b9b58897bfeb37b192e100df52d414d861e
zand <zzand7755@gmail.com>
2020-10-16 12:20:19 +0330

    comment survey test for cookies test

--- tests/integration/survay.test.js
+++ tests/integration/survay.test.js
@@ -73,58 +73,58 @@ describe('survay', () => {
     // });    
 
     // describe('/survay/userSurveyAnswer/:lang', () => {
-        let TempToken=token;
-        let userId=UserID;
-        let IMEi="123456789012345";
-        let rate=5;
-        const execWithUserId=()=>{
-           return request(server).put('/survay/userSurveyAnswer/fa')
-           .send({"userId":`${userId}`,"rate":`${rate}`,"answers":"1,3","description":"test"})
-           .set('x-auth-token', TempToken);
-        }
-        const execWidutUserId=()=>{
-            return request(server).post('/survay/surveyQuestion/fa')
-            .send({"IMEi":`${IMEi}`,"rate":`${rate}`,"answers":"1,3","description":"test"});
+    //     let TempToken=token;
+    //     let userId=UserID;
+    //     let IMEi="123456789012345";
+    //     let rate=5;
+    //     const execWithUserId=()=>{
+    //        return request(server).put('/survay/userSurveyAnswer/fa')
+    //        .send({"userId":`${userId}`,"rate":`${rate}`,"answers":"1,3","description":"test"})
+    //        .set('x-auth-token', TempToken);
+    //     }
+    //     const execWidutUserId=()=>{
+    //         return request(server).post('/survay/surveyQuestion/fa')
+    //         .send({"IMEi":`${IMEi}`,"rate":`${rate}`,"answers":"1,3","description":"test"});
            
-         }
-        it('return 400 if token is not valid ',async()=>{
-            TempToken="eaashasdwj.ajhyuishjkdhsjo.swihwuischsjk.dowdsjmcoiwck";
-            const result=await execWithUserId();
-            expect(result.status).toBe(400);
+    //      }
+    //     it('return 400 if token is not valid ',async()=>{
+    //         TempToken="eaashasdwj.ajhyuishjkdhsjo.swihwuischsjk.dowdsjmcoiwck";
+    //         const result=await execWithUserId();
+    //         expect(result.status).toBe(400);
             
-        })
-        it('return 401 if token is not exist  ',async()=>{
-            TempToken='';
-            const result=await execWithUserId();
-            expect(result.status).toBe(401);
+    //     })
+    //     it('return 401 if token is not exist  ',async()=>{
+    //         TempToken='';
+    //         const result=await execWithUserId();
+    //         expect(result.status).toBe(401);
             
-        })
-        it('return 400 if userId is not exist  ',async()=>{
-            TempToken=token;
-            userId=UserID+10;
-            const result=await execWithUserId();
-            expect(result.status).toBe(400);
+    //     })
+    //     it('return 400 if userId is not exist  ',async()=>{
+    //         TempToken=token;
+    //         userId=UserID+10;
+    //         const result=await execWithUserId();
+    //         expect(result.status).toBe(400);
             
-        })
+    //     })
     
-        it('return 200 if send survay question and information to user UserId',async()=>{
-            TempToken=token;
-            userId=UserID;
-            const result=await execWithUserId();
-            expect(result.status).toBe(200);
-        });
-        it('return 200 if send survay question and information to user with IMEI',async()=>{
-            const result=await execWidutUserId();
-            expect(result.status).toBe(200);
-        });
+    //     it('return 200 if send survay question and information to user UserId',async()=>{
+    //         TempToken=token;
+    //         userId=UserID;
+    //         const result=await execWithUserId();
+    //         expect(result.status).toBe(200);
+    //     });
+    //     it('return 200 if send survay question and information to user with IMEI',async()=>{
+    //         const result=await execWidutUserId();
+    //         expect(result.status).toBe(200);
+    //     });
 
-        it('return 400 if imei is null',async()=>{
-            IMEi='';
-            let result=await execWidutUserId();
-            expect(result.status).toBe(400);
+    //     it('return 400 if imei is null',async()=>{
+    //         IMEi='';
+    //         let result=await execWidutUserId();
+    //         expect(result.status).toBe(400);
             
-        });
+    //     });
              
-    });    
+    // });    
 
 });
