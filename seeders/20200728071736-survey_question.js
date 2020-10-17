fb6bb3f13528e0d64a012a1bcffe0d24c5820e9e
zand <zzand7755@gmail.com>
2020-10-17 16:37:48 +0330

    fix survey apis and update swager

---
+++ seeders/20200728071736-survey_question.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('survey_question', [
+      { question: 'کارکردن با پرتو برام راحته',weakness:0, createdAt: new Date(), updatedAt: new Date() },
+      { question: ' از پس تشخیص روزهای مهم دوره هام برمیاد',weakness:0, createdAt: new Date(), updatedAt: new Date() },
+      { question: 'ظاهر پرتو جذاب و دوست داشتنیه',weakness:0, createdAt: new Date(), updatedAt: new Date() },
+      { question: 'امکانات متنوع و خوبی داره',weakness:0, createdAt: new Date(), updatedAt: new Date() },
+      { question: 'برنامه اختلال داره و گاهی هنگ می کنه',weakness:1, createdAt: new Date(), updatedAt: new Date() },
+      { question: 'باوجود وارد کردن اطلاعاتم بازهم محاسبات پرتو اشتباهه',weakness:1, createdAt: new Date(), updatedAt: new Date() },
+      { question: 'کارکردن با پرتو برام سخته',weakness:1, createdAt: new Date(), updatedAt: new Date() },
+      { question: 'طراحی برنامه قشنگ نیست',weakness:1, createdAt: new Date(), updatedAt: new Date() },
+      
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('survey_questions', null, {});
+  }
+};
