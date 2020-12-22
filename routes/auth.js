ed20161436978f41def77fe01177eefd4a0eddcb
zand <zzand7755@gmail.com>
2020-12-22 22:45:52 +0330

    change

--- routes/auth.js
+++ routes/auth.js
@@ -102,6 +102,7 @@ router.post("/logIn/:lang", async (req, res) => {
     version: req.body.version,
     login_date: Date.now(),
   });
+  console.log("req.header(app-type)",req.header("app-type"));
   if (RegExp('http://localhost:3925').test(req.headers['origin']) == true) {
     return res.status(200).json({ data: { id: usr.id, token: token, userName: usr.name, type: usr.version_type } });
   }
