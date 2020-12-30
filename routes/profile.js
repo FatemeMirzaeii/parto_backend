d7504d07e59adda26f3a0ea22a3a87413f05fc38
zand <zzand7755@gmail.com>
2020-12-30 12:55:40 +0330

    change

--- routes/profile.js
+++ routes/profile.js
@@ -281,7 +281,7 @@ router.post("/setLastSyncTime/:userId/:lang", auth, async (req, res) => {
 router.get("/syncProfile/:userId/:syncTime/:lang", auth, async (req, res) => {
   let usr = await user.findByPk(req.params.userId);
   if (usr == null) return res.status(400).json({ message: await translate("INVALIDENTRY", req.params.lang) });
-
+  
   let usrID;
   if (usr.partner_id != null) {
     usrID = usr.partner_id
@@ -290,8 +290,11 @@ router.get("/syncProfile/:userId/:syncTime/:lang", auth, async (req, res) => {
     usrID = usr.id
   }
 
+  let userProf=await user_profile.findByPk(req.params.usrID);
+  if(userProf==null) return res.status(200).json({ data: usrProf });
+
   let syncTime;
-  if (req.params.syncTime ==null) {
+  if (req.params.syncTime == "null") {
     syncTime = await user_profile.findOne({
       attributes: ['updatedAt'],
       where: {
