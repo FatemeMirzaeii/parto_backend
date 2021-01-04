a0dcbaf274665a66229b0160ef9caeed192dc807
zand <zzand7755@gmail.com>
2021-01-04 11:34:41 +0330

    fix test error

--- tests/unit/middleware/auth.test.js
+++ tests/unit/middleware/auth.test.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const auth=require('../../../middleware/auth');
 var fs = require("fs");
 const secret = fs.readFileSync("../private.key", "utf8");
+require('mysql2/node_modules/iconv-lite').encodingExists('foo');
 
 describe("user.generateAuthToken", () => {
   it("should return a valid JWT", async() => {
