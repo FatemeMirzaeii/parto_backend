aa7b920f3d44d79337ddf6112569f9b718059223
zand <zzand7755@gmail.com>
2021-04-02 08:18:19 +0430

    change phone number regex

--- routes/auth.js
+++ routes/auth.js
@@ -55,7 +55,7 @@ router.post("/logIn/:lang", async (req, res) => {
     return res.status(400).json({ message: await translate("INVALIDENTRY", req.params.lang) });
   }
   if (req.body.phone != "") {
-    const regex = RegExp(/+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/g);
+    const regex = RegExp(/^(?:[0-9] ?){5,13}[0-9]$/);
     let check = regex.test(req.body.phone);
     console.log("check", check)
     if (!check) return res.status(400).json({ message: await translate("INVALIDENTRY", "fa") });
