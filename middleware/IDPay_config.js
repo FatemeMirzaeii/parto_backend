b0a254aa20df512d0b06d90b4fdd83b2b3147512
zand <zzand7755@gmail.com>
2021-05-30 13:41:24 +0430

    change

--- middleware/IDPay_config.js
+++ middleware/IDPay_config.js
@@ -1,6 +1,6 @@
 let config = {
 	url: 'https://api.idpay.ir/v1.1/payment',
-	key: '646b79e4-9073-48be-98b1-1499b95dde6e',
+	key: 'aa320c8c-43c7-4d21-aa0c-2eb0e476eecb',
     test_key:'646b79e4-9073-48be-98b1-1499b95dde6e',
 	API: {
 		PV: '/verify',
