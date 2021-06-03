fedb69744984106318d2e453a7b3e4f5e044591c
zand <zzand7755@gmail.com>
2021-06-03 15:43:04 +0430

    delete test db and added

--- seeders/20210418114328-services.js
+++ seeders/20210418114328-services.js
@@ -3,13 +3,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('service', [
-      { name: 'Talk to specialist', type: 'virtual S', amount: 5000, created_at: new Date(), updated_at: new Date() },
-      { name: 'Analysis', type: 'virtual S', amount: 2000, created_at: new Date(), updated_at: new Date() },
-      { name: 'Increase credit 10000', type: 'virtual S', amount: 10000, created_at: new Date(), updated_at: new Date() },
-      { name: 'Increase credit 15000', type: 'virtual S', amount: 140000, created_at: new Date(), updated_at: new Date() },
-      { name: 'Increase credit 20000', type: 'virtual S', amount: 190000, created_at: new Date(), updated_at: new Date() },
-      { name: 'Increase credit 50000', type: 'virtual S', amount: 470000, created_at: new Date(), updated_at: new Date() },
-      { name: 'Increase credit 100000', type: 'virtual S', amount: 950000, created_at: new Date(), updated_at: new Date() },
+      { name: 'صحبت با مشاور', type: 'virtual S', amount: 100000, created_at: new Date(), updated_at: new Date() },
+      { name: 'افزایش اعتبار به  ارزش 10000 ریال', type: 'virtual S', amount: 10000, created_at: new Date(), updated_at: new Date() },
+      { name: 'افزایش اعتبار به  ارزش 100000 ریال', type: 'virtual S', amount: 100000, created_at: new Date(), updated_at: new Date() },
+      { name: 'افزایش اعتبار به  ارزش 200000 ریال', type: 'virtual S', amount: 200000, created_at: new Date(), updated_at: new Date() },
+      { name: 'افزایش اعتبار به  ارزش 500000 ریال', type: 'virtual S', amount: 500000, created_at: new Date(), updated_at: new Date() },
+      { name: 'افزایش اعتبار به  ارزش 1000000 ریال', type: 'virtual S', amount: 1000000, created_at: new Date(), updated_at: new Date() },
     ], {});
   },
   down: (queryInterface, Sequelize) => {
