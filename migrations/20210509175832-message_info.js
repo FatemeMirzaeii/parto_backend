9e476efb36b554916584acad01efc633cfe586a4
zand <zzand7755@gmail.com>
2021-06-03 16:07:44 +0430

    change

--- migrations/20210509175832-message_info.js
+++ migrations/20210509175832-message_info.js
@@ -29,7 +29,7 @@ module.exports = {
       status:{
         type: Sequelize.BOOLEAN ,
         allowNull: false,
-        default:0
+        default:false
       },
       total_question:{
         type: Sequelize.INTEGER ,
