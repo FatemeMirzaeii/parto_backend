46764e36c45755a912a90fc07405df254ce36990
zand <zzand7755@gmail.com>
2021-08-02 20:04:24 +0430

    add bank modle

---
+++ models/bank.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const bank = sequelize.define('bank_receipt', {
+
+        order_id: DataTypes.STRING,
+        authority: DataTypes.STRING,
+        gateway_link: DataTypes.STRING,
+        meta_data: DataTypes.TEXT,
+        gateway: DataTypes.ENUM('zp', 'ID_pay'),
+        status: DataTypes.ENUM('Success', 'UnSuccess', 'Reversed', 'Waiting', 'Cancel'),
+
+    }, {
+        freezeTableName: true,
+        underscored: true,
+    });
+    bank.associate = function (models) {
+        bank.belongsTo(models.invoice, {
+            onDelete: "CASCADE",
+        })
+    };
+    return bank;
+};
