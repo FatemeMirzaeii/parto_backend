bceb263ed5829953aabc42c8b3363031bb796551
zand <zzand7755@gmail.com>
2021-10-12 22:36:27 +0330

    change ci/cd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,11 @@ module.exports = {
   apps : [{
     name: "development",
     script: "./development.js",
+    instances: 1,
+    cron_restart: '0 0 * * *',
+    watch: true,
+    max_memory_restart: '300M',
+    exp_backoff_restart_delay: 100,
     env: {
       NODE_ENV: "test",
     }
@@ -9,6 +14,11 @@ module.exports = {
   {
     name: "app",
     script: "./app.js",
+    instances: 1,
+    cron_restart: '0 0 * * *',
+    watch: true,
+    max_memory_restart: '300M',
+    exp_backoff_restart_delay: 100,
     env: {
       NODE_ENV: "production",
     },
