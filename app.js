9a7e7038032752b4ed6ef0b767b96ded99d36968
zand <zzand7755@gmail.com>
2021-10-23 14:55:29 +0330

    comment rate limit

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ app.use(cors({
   exposedHeaders: 'x-auth-token'
 }));
 
-const authenticatedLimiter = rateLimit({
-  windowMs: 1000, // 1 second window
-  max: 50, // start blocking after 10 requests
-  message:
-  { message: "تعداد درخواست های شما در چند دقیقه گذشته بیش از حد مجاز بوده است، لطفا پس از چند دقیقه دوباره امتحان کنید "},
-  headers: true,
-});
+// const authenticatedLimiter = rateLimit({
+//   windowMs: 1000, // 1 second window
+//   max: 50, // start blocking after 10 requests
+//   message:
+//   { message: "تعداد درخواست های شما در چند دقیقه گذشته بیش از حد مجاز بوده است، لطفا پس از چند دقیقه دوباره امتحان کنید "},
+//   headers: true,
+// });
 
 app.use("/cycle", authenticatedLimiter);
 app.use("/pregnancy", authenticatedLimiter);
@@ -61,13 +61,13 @@ app.use("/payment", authenticatedLimiter);
 app.use("/message", authenticatedLimiter);
 
 
-const unauthenticatedLimiter = rateLimit({
-  windowMs: 2*60 * 1000, // 2 minet window
-  max: 15, // start blocking after 1 requests
-  message:
-  { message: "تعداد درخواست های شما در چند دقیقه گذشته بیش از حد مجاز بوده است، لطفا پس از چند دقیقه دوباره امتحان کنید "},
-  headers: true,
-});
+// const unauthenticatedLimiter = rateLimit({
+//   windowMs: 2*60 * 1000, // 2 minet window
+//   max: 15, // start blocking after 1 requests
+//   message:
+//   { message: "تعداد درخواست های شما در چند دقیقه گذشته بیش از حد مجاز بوده است، لطفا پس از چند دقیقه دوباره امتحان کنید "},
+//   headers: true,
+// });
 app.use("/auth", unauthenticatedLimiter);
 app.use("/contactUs", unauthenticatedLimiter);
 app.use("/survey", authenticatedLimiter);
